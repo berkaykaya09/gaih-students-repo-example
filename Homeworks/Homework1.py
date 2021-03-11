f51acfe7949b8d2456f2730271959cbfb9073009
berkaykaya09 <80210343+berkaykaya09@users.noreply.github.com>
2021-03-11 19:10:11 +0300

    Add files via upload

---
+++ Homeworks/Homework1.py
@@ -0,0 +1,12 @@
+#Homework 1
+list1 = [1,3,5,7,9]
+list2 = [0,2,4,6,8]
+list1.extend(list2)
+list1.sort()
+
+list1=[x*2 for x in list1]
+print(list1)
+
+for i in list1:
+    i+=1
+    i=print(type(i))
