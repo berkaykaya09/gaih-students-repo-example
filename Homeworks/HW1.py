f2493b45f1b13016b6709d207462e99dc1106cc4
berkaykaya09 <80210343+berkaykaya09@users.noreply.github.com>
2021-03-11 19:07:54 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,12 @@
-#Explain your work
+#Homework 1
+list1 = [1,3,5,7,9]
+list2 = [0,2,4,6,8]
+list1.extend(list2)
+list1.sort()
 
-#Question 1
-for x in range(a):
-	print(a)
+list1=[x*2 for x in list1]
+print(list1)
+
+for i in list1:
+    i+=0
+    i=print(type(i))
